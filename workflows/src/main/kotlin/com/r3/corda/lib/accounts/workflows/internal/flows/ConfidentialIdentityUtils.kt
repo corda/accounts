ad5eb2ee9836fcb5b3565e2fc12f7dc8a9e93985
Xavier Leprêtre <xavierlepretre@users.noreply.github.com>
2020-05-19 00:06:59 +0400

    Made SendKeyForAccountFlow return the created key too.
    Added tests.

--- workflows/src/main/kotlin/com/r3/corda/lib/accounts/workflows/internal/flows/ConfidentialIdentityUtils.kt
+++ workflows/src/main/kotlin/com/r3/corda/lib/accounts/workflows/internal/flows/ConfidentialIdentityUtils.kt
@@ -6,12 +6,6 @@ import net.corda.core.identity.AnonymousParty
 import net.corda.core.node.ServiceHub
 import net.corda.core.serialization.CordaSerializable
 
-@CordaSerializable
-internal enum class AccountSearchStatus {
-    FOUND,
-    NOT_FOUND
-}
-
 @CordaInternal
 fun ServiceHub.createKeyForAccount(accountInfo: AccountInfo) : AnonymousParty {
     val newKey = this.keyManagementService.freshKey(accountInfo.identifier.id)
