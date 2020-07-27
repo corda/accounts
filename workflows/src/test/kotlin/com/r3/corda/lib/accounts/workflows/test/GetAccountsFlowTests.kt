8c4d2269050836c459926106af7726cc610ba294
Ionut Musat <imusat@ledgertech.com>
2020-07-27 12:55:11 +0300

    Add PageSpecification to some vault queries
    If no `PageSpecification` is provided to the `VaultService.queryBy(...)` function an exception is thrown if the number of results exceeds the `DEFAULT_PAGE_SIZE` vault configuration.

--- workflows/src/test/kotlin/com/r3/corda/lib/accounts/workflows/test/GetAccountsFlowTests.kt
+++ workflows/src/test/kotlin/com/r3/corda/lib/accounts/workflows/test/GetAccountsFlowTests.kt
@@ -5,6 +5,8 @@ import com.r3.corda.lib.accounts.workflows.flows.CreateAccount
 import com.r3.corda.lib.accounts.workflows.flows.RequestAccountInfo
 import com.r3.corda.lib.accounts.workflows.flows.ShareAccountInfo
 import com.r3.corda.lib.accounts.workflows.internal.accountService
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.node.services.vault.DEFAULT_PAGE_SIZE
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.common.internal.testNetworkParameters
 import net.corda.testing.node.MockNetwork
@@ -17,6 +19,7 @@ import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import java.util.*
 import kotlin.test.assertEquals
 
@@ -82,6 +85,65 @@ class GetAccountsFlowTests {
             Assert.assertThat(accountService.allAccounts(), containsInAnyOrder(account1, account2, account3, account4))
         }
 
+    }
+
+
+    @Test
+    fun `should lookup all hosted accounts when the hosted accounts number exceeds DEFAULT_PAGE_SIZE`() {
+        val hostedAccounts = mutableListOf<StateAndRef<AccountInfo>>()
+        (1..(DEFAULT_PAGE_SIZE + 1)).forEach {
+            hostedAccounts.add(a.startFlow(CreateAccount("Test_AccountA_$it")).runAndGet(network))
+        }
+        val account4 = b.startFlow(CreateAccount("Stefano_Account3")).runAndGet(network)
+
+        b.startFlow(ShareAccountInfo(account4, listOf(a.identity()))).runAndGet(network)
+
+        val accountService = a.services.accountService
+
+        a.transaction {
+            assertDoesNotThrow { val a = accountService.ourAccounts() }
+            Assert.assertThat(accountService.accountInfo(account4.uuid), `is`(account4))
+            Assert.assertThat(accountService.ourAccounts(), containsInAnyOrder(*hostedAccounts.toTypedArray()))
+            Assert.assertThat(accountService.ourAccounts(), not(hasItem(account4)))
+        }
+
+    }
+
+    @Test
+    fun `should lookup all accounts when the number of accounts exceeds DEFAULT_PAGE_SIZE`() {
+        val account1 = a.startFlow(CreateAccount("A_Account1")).runAndGet(network)
+        val account2 = a.startFlow(CreateAccount("A_Account2")).runAndGet(network)
+        val account3 = a.startFlow(CreateAccount("A_Account3")).runAndGet(network)
+        val accountsHostedByB = mutableListOf<StateAndRef<AccountInfo>>()
+        (1..DEFAULT_PAGE_SIZE).forEach {
+            val account = b.startFlow(CreateAccount("B_Account$it")).runAndGet(network)
+            accountsHostedByB.add(account)
+            b.startFlow(ShareAccountInfo(account, listOf(a.identity()))).runAndGet(network)
+        }
+
+        val accountService = a.services.accountService
+
+        a.transaction {
+            assertDoesNotThrow { accountService.allAccounts() }
+            Assert.assertThat(accountService.allAccounts(), containsInAnyOrder(*(listOf(account1, account2, account3) + accountsHostedByB).toTypedArray()))
+        }
+    }
+
+    @Test
+    fun `should lookup all accounts hosted by another node when the number of accounts exceeds DEFAULT_PAGE_SIZE`() {
+        val accountsHostedByB = mutableListOf<StateAndRef<AccountInfo>>()
+        (1..(DEFAULT_PAGE_SIZE + 1)).forEach {
+            val account = b.startFlow(CreateAccount("B_Account$it")).runAndGet(network)
+            accountsHostedByB.add(account)
+            b.startFlow(ShareAccountInfo(account, listOf(a.identity()))).runAndGet(network)
+        }
+
+        val accountService = a.services.accountService
+
+        a.transaction {
+            assertDoesNotThrow { accountService.accountsForHost(b.identity()) }
+            Assert.assertThat(accountService.accountsForHost(b.identity()), containsInAnyOrder(*(accountsHostedByB).toTypedArray()))
+        }
     }
 
     @Test
